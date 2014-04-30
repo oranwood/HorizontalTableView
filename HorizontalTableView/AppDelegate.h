d1ebfd762d5236320d3e3a1e93e376275dab5aa8
wuzhangfu <wuzhangfu@smartisan.cn>
2014-04-30 13:29:45 +0800

    Initial Commit

---
+++ HorizontalTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HorizontalTableView
+//
+//  Created by wuzhangfu on 14-4-30.
+//  Copyright (c) 2014年 Smartisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HorizontalTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HorizontalTableView
+//
+//  Created by wuzhangfu on 14-4-30.
+//  Copyright (c) 2014年 Smartisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
